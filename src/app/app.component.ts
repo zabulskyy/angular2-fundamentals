7a7f9a871f4a64492d45b8a6164fd1a6ca1c7be8
vzabulskyy <v.zabulskyy@gmail.com>
2017-06-08 17:00:27 +0300

    minor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `<div>
+    <app-simple-form></app-simple-form>
+  </div>`
 })
 export class AppComponent {
-  title = 'app';
+  title = 'app works';
 }
